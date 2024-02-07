84d53da5576fafa1e9865768ed01b331df85c3e3
Davide Rodo <dcl.simpson@gmail.com>
2024-02-08 00:28:46 +0100

    chore(): updated imports

--- spotify-anilist/src/app/app.routes.ts
+++ spotify-anilist/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {SpotifyLoginComponent} from "./spotify-login/spotify-login.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{path: "", component: SpotifyLoginComponent}
+];
