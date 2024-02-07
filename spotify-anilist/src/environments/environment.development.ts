d434f3f621f529984ddcbd649735b2e1974d6cf1
Davide Rodo <dcl.simpson@gmail.com>
2024-02-08 00:29:00 +0100

    chore: updated enviroments and packages

---
+++ spotify-anilist/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+
+	spotify_app_id: "24c32ebfcce24bc7bdefcd2180c290fb",
+	spotify_app_secret: "25f59c3f3ddf43298cafeaa6bbc1b9d7",
+	spotify_token_url: "https://accounts.spotify.com/api/token"
+};
